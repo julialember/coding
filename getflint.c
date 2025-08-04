6bb559145c8dc34fe70b0d0b24d693a8ea2a5813
julialember <pizzabosew@mail.ru>
2025-08-04 13:24:07 +0300

    getfloat 5.2

--- getflint.c
+++ getflint.c
@@ -6,10 +6,10 @@ int buf_len = 0;
 void ungetch(int);
 int getch(void);
 int getint(int *);
-
+float getfloat(float *); 
 int main(){
-    int num = 0;
-    if (getint(&num)) printf("%d", num);
+    float f;
+    if (getfloat(&f)) printf("%g\n", f);
 }
 
 int getch(void) {
@@ -19,6 +19,36 @@ int getch(void) {
 void ungetch(int c) {
     if (buf_len >= MAXBUFFER) printf("the stack is already full");
     else buffer[buf_len++] = c; 
+}
+
+float  getfloat(float *pn) {
+    int c, sign;
+    while (isspace(c = getch()));
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);
+        return 0; 
+    }
+    sign = ( c == '-' ) ? -1 : 1; 
+    if ( c == '-' || c == '+' ){
+        int next_char = getch();
+        if (!isdigit(next_char)){
+            ungetch(next_char);
+            ungetch(c); 
+            return 0;
+        }
+        c = next_char; 
+    } 
+    
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    if ( c == '.' ) {
+        float power = 0.0; 
+        for (float div = 10; isdigit(c = getch()); div *= 10) power += (c - '0')/div;
+        *pn += power;
+    }
+    *pn *= sign;
+    if ( c != EOF ) ungetch(c);
+    return c; 
 }
 
 int getint(int *pn) {
@@ -45,3 +75,4 @@ int getint(int *pn) {
     if ( c != EOF ) ungetch(c);
     return c; 
 }
+
