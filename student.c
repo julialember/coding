14f155bf22bbdff8ef3b1601e4e2e1c9ff5cebf2
julialember <pizzabosew@mail.ru>
2025-08-08 14:38:12 +0300

    student ver 2.

--- student.c
+++ student.c
@@ -1,91 +1,101 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct {
-    char *name;
-    int age;
-    float *grades;
-    int grades_len;
-} Student;
+#define MAXNAME 40
+#define MAXVALS 100
+int buffer[MAXVALS];
+int buf_len = 0;
 
-#define MAXBUFFER 100
-int buffer[MAXBUFFER];
-int buf_len = 0; 
-int getch(void) {
-    return buf_len > 0 ? buffer[--buf_len] : getchar(); 
+int getch(void){
+    return buf_len > 0 ? buffer[--buf_len] : getchar();   
 }
 
 void ungetch(int c) {
-    if (buf_len >= MAXBUFFER) printf("the stack is full");
-    else buffer[buf_len++] = c;
+    if (buf_len >= MAXVALS) printf("the stack is full\n");
+    else buffer[buf_len++] = c; 
 }
-int get_floats(float**ptr){
-    int i = 0, c; 
-    float *result = NULL;
-    while ((c = getch()) != '\n' && c != EOF){
+
+int get_floats(float**ptr) {
+    int c, len = 0; 
+    *ptr = NULL; 
+    while ((c = getch()) != '\n' && c != EOF) {
         while (isspace(c) && c != '\n') c = getch();
         if (!isdigit(c) && c != EOF) {
             ungetch(c);
-            return i;
+            break;
         }
-        result = realloc(result, sizeof(float) * (i + 1)); 
-        if (!result) {
-            free(result);
-            printf("problem with mem allocated");
+        *ptr = realloc(*ptr, sizeof(float) * (len + 1));
+        if (!*ptr){
+            free(ptr);
+            printf("problems with memory allocated");
             return -1; 
         }
-        for (result[i] = 0; isdigit(c); c = getch()) result[i] = result[i] * 10 + ( c - '0'); 
-        if ( c == '.') {
+        for ((*ptr)[len] = 0; isdigit(c); c = getch()) (*ptr)[len] = (*ptr)[len] * 10 + ( c - '0' ); 
+        if ( c == '.' ) {
             float power = 1.0; 
-            for (c = getch(); isdigit(c); power *= 10, c = getchar()) result[i] = result[i] * 10 + ( c - '0');
-            result[i]/=power; 
+            for (c = getch(); isdigit(c); c = getch(), power*=10) (*ptr)[len] = (*ptr)[len] * 10 + ( c - '0');
+            (*ptr)[len]/=power; 
         }
-        i++;
-        while ( c == ' ' || c == '\t' ) c = getch();
-        if ( c != EOF) ungetch(c);
-    } 
-    *ptr = result;
-    return i;
+        len++; 
+        while ( c == ' ' || c == '\t' ) c = getch(); 
+        if ( c != EOF ) ungetch(c);
+    }
+    return len;
 }
 
-int getname(char *s) {
-    int i = 0; 
-    for (; (s[i] = getchar()) != '\n'; i++);
-    return i; 
+typedef struct {
+    char* name; 
+    int age; 
+    float* grades; 
+    int grades_count; 
+} Student; 
+
+void copycat(char *from, char* to) {
+    if (!from || !to) return; 
+    while (*from) *to++ = *from++;
+    *to++ = '\0';
 }
 
-Student* createStudent(){
+int get_name(char *name) {
+    int len = 0;
+    for (int c = getchar(); c != '\n' && len < MAXVALS - 1 ; c = getchar()) name[len++] = c; 
+    return len;
+}
+
+Student* createStudent(void) {
     Student* new = malloc(sizeof(Student));
-    printf("let me know name: "); 
-    char s[30];
-    new->name = malloc(getname(s) + 1);
-    strcpy(new->name, s);
-    printf("let me know your age: "); scanf("%d", &new->age);
-    while (getchar() != '\n');
-    printf("let me know grades: "); new->grades_len =  get_floats(&new->grades); 
-    if (new->grades_len < 0) {
-        free(new->name);
-        free(new);
+    printf("let me know the name: ");
+    char name[MAXVALS];
+    new->name = malloc(get_name(name) + 1);
+    copycat(name, new->name);
+    printf("let me know age: ");
+    scanf("%d", &new->age);
+    while(getchar() != '\n'); 
+    printf("let me know grades (sep with space)\n"); 
+    if ((new->grades_count = get_floats(&new->grades)) < 0 || !*new->name) {
+        free(new->name); 
+        free(new->grades); 
+        free(new); 
+        printf("cant allocate the memory");
         return NULL;
     }
     return new;
 }
 
-void printStudent(const Student* st) {
-    printf("name: %sage: %d\ngrades:\n", st->name, st->age);
-    for (int i = 0; i < st->grades_len; i++) printf("%g ", st->grades[i]); 
-}
-
-void freeStudent(Student *st) {
-    if (!st) return; 
+void freeStudent(Student* st) {
     free(st->name); 
-    free(st->grades);
+    free(st->grades); 
     free(st);
 }
+
+void printStudent(Student* st){
+    printf("name: %s\nage: %d\ngrades:\n", st->name, st->age);
+    for (int i = 0; i < st->grades_count; i++) printf("%g ", (st->grades)[i]);
+}
+
 int main() {
-    Student *new = createStudent();
-    printStudent(new);
-    freeStudent(new);
+    Student* st = createStudent();
+    printStudent(st);
+    freeStudent(st);
 }
