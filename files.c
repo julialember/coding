115d3d1f303aa592f5140819b0c22b871623b7db
julialember <pizzabosew@mail.ru>
2025-08-10 11:47:08 +0300

    cmp v1.

--- files.c
+++ files.c
@@ -3,26 +3,34 @@
 #include <string.h>
 #define MAXLEN 40
 
-int cmpLines(char **answer, FILE* f, int max_len) {
+int getfline(char **answer, FILE* f, int max_len) {
     if (!f || max_len <= 0 || !answer) return -1; 
     if (*answer) free(*answer); 
-    *answer = NULL; 
     *answer = malloc(max_len);
     if (!*answer) return -1;
     int c, len = 0; 
     while (len < max_len - 1 && (c = getc(f)) != EOF && c != '\n') (*answer)[len++] = c; 
-    while ((*answer)[len] == ' ' || (*answer)[len] == '\t') {
-        (*answer)--; len--; 
-    } 
     if (len == 0 || c == EOF) {
         free(*answer);
         *answer = NULL; 
         return -1; 
-    } 
-    (*answer)[len] = '\0';
+    }  
+    for(len--; ((*answer)[len] == ' ' || (*answer)[len] == '\t') && len > 0; (*answer)[len--] = '\0');
+    *answer = realloc(*answer, len + 1);  
+    (*answer)[len++] = '\0';
     return len; 
+}
+
+void cmpFiles(FILE* f, FILE* s) {
+    if (!f || !s) return;
+    char* fir = NULL; 
+    char* sec = NULL;
+    int fl = getfline(&fir, f, MAXLEN);
+    int fs = getfline(&sec, s, MAXLEN);
+    if (fl != fs) printf("%s", fir?fir:sec?sec:"files are the same"); 
 }
 int main() {
     FILE* text1 = fopen("text1.txt", "r");
     FILE* text2 = fopen("text.txt", "r");
+    cmpFiles(text1, text2);
 }
