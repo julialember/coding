85a90697a89d2d2b5374e468f4495f47f856bd8a
с T <ct@MacBook-Pro-s.local>
2025-08-10 11:21:15 +0300

    files v 2.

--- files.c
+++ files.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MAXLEN 40
 
-int main(int argc, char *argv[]) {
-    FILE *fp; 
-    void filecopy(FILE*, FILE*);
-
-    char *prog = argv[0];
-    if (argc == 1)
-        filecopy(stdin, stdout);
-    else 
-        while (--argc > 0)
-            if ((fp = fopen(*++argv, "r")) == NULL){
-                fprintf(stderr, "%s: cant opent the file %s\n", 
-                        prog, *argv);
-                exit(1);
-            } else {
-                filecopy(fp, stdout);
-                fclose(fp);
-            }
-    if (ferror(stdout)) {
-        fprintf(stderr, "%s error write in stdout\n", prog);
-        exit(2); 
-    }
-    return 0; 
+int cmpLines(char **answer, FILE* f, int max_len) {
+    if (!f || max_len <= 0 || !answer) return -1; 
+    if (*answer) free(*answer); 
+    *answer = NULL; 
+    *answer = malloc(max_len);
+    if (!*answer) return -1;
+    int c, len = 0; 
+    while (len < max_len - 1 && (c = getc(f)) != EOF && c != '\n') (*answer)[len++] = c; 
+    while ((*answer)[len] == ' ' || (*answer)[len] == '\t') {
+        (*answer)--; len--; 
+    } 
+    if (len == 0 || c == EOF) {
+        free(*answer);
+        *answer = NULL; 
+        return -1; 
+    } 
+    (*answer)[len] = '\0';
+    return len; 
 }
-
-void filecopy(FILE* ifp, FILE* ofp) {
-    int c;
-    while ((c = getc(ifp)) != EOF) 
-        putc(c, ofp);
+int main() {
+    FILE* text1 = fopen("text1.txt", "r");
+    FILE* text2 = fopen("text.txt", "r");
 }
-
